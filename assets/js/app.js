fb2acb7a523d5a9cd794722fb1afae632b2c6a5f
Jhonatan David <82552187+jotahdavid@users.noreply.github.com>
2024-02-14 19:07:04 -0300

    feat(progress-bar): retrieve info by options.json

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,7 +9,9 @@ function bootstrap() {
   $skipVideoButton?.addEventListener('click', handleSkipVideoButton);
 
   const $video = document.querySelector('.intro video');
-  $video.addEventListener('ended', handleVideoEnded);
+  $video?.addEventListener('ended', handleVideoEnded);
+
+  retrieveInfoData();
 }
 
 function handleEnterSiteButtonClick() {
@@ -36,6 +38,66 @@ function handleSkipVideoButton() {
 
 function handleVideoEnded() {
   handleSkipVideoButton();
+}
+
+async function retrieveInfoData() {
+  const $infos = document.querySelectorAll('[data-info]');
+  if ($infos.length === 0) return;
+
+  const infoData = await fetchInfoData();
+
+  $infos.forEach(function ($info) {
+    const info = $info.dataset.info;
+    if (!info) return;
+
+    const $total = $info.querySelector('[data-info-text="total"]');
+    $total.textContent = new Intl.NumberFormat().format(infoData[info]);
+
+    const $max = $info.querySelector('[data-info-text="max"]');
+    $max.textContent = formatNumber(infoData.max[info]);
+  });
+
+  const $progressBar = document.querySelector('.progress-bar');
+  $progressBar.style.setProperty('--progress-bar-percentage', getPercentageFromInfo(infoData) + '%');
+
+  function formatNumber(number) {
+    let result = String(number);
+    if (number >= 1_000_000) {
+      result = result.slice(0, result.length - 6);
+      return `${result}kk`;
+    }
+    if (number >= 1_000) {
+      result = result.slice(0, result.length - 3);
+      return `${result}k`;
+    }
+    return result;
+  }
+
+  function getPercentageFromInfo(info) {
+    const attributes = Object.keys(info.max);
+
+    const sumPercentage = attributes.reduce(function (acc, attribute) {
+      const percentage = Math.min(info[attribute] * 100 / info.max[attribute], 100);
+      return acc + percentage;
+    }, 0);
+
+    const result = sumPercentage / attributes.length;
+
+    return result;
+  }
+}
+
+async function fetchInfoData() {
+  const infoDataUrl = window.INFO_DATA_URL ?? './options.json';
+  const response = await fetch(infoDataUrl);
+  const infoData = await response.json();
+  const parsedData = {
+    max: {
+      ...(infoData.quests[`#${infoData.data.currentQuest}`] ?? infoData.quests['#1']),
+    },
+    ...infoData.data,
+  };
+  return parsedData;
 }
 
 document.addEventListener('DOMContentLoaded', bootstrap);
