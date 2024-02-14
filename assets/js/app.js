2b2b2d82961515c116eaed0988e378c38b803584
Jhonatan David <82552187+jotahdavid@users.noreply.github.com>
2024-02-13 22:25:45 -0300

    feat(intro): create intro section

---
+++ assets/js/app.js
@@ -0,0 +1,31 @@
+function bootstrap() {
+  const $enterSiteButton = document.querySelector('.intro__welcome__button');
+  $enterSiteButton?.addEventListener('click', handleEnterSiteButtonClick);
+
+  const $fullScreenButton = document.querySelector('.intro__video__full-screen');
+  $fullScreenButton?.addEventListener('click', handleFullScreenButton);
+
+  const $skipVideoButton = document.querySelector('.intro__video__skip');
+  $skipVideoButton?.addEventListener('click', handleSkipVideoButton);
+}
+
+function handleEnterSiteButtonClick() {
+  const $welcomeIntro = document.querySelector('.intro__welcome');
+  $welcomeIntro.remove();
+
+  const $video = document.querySelector('.intro video');
+  $video.play();
+}
+
+function handleFullScreenButton() {
+  const $video = document.querySelector('.intro video');
+  $video.requestFullscreen();
+}
+
+function handleSkipVideoButton() {
+  const $intro = document.querySelector('.intro');
+  $intro.remove();
+  document.documentElement.classList.remove('no-scroll');
+}
+
+document.addEventListener('DOMContentLoaded', bootstrap);
